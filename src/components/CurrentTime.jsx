bfc3eb93b7c099189bf0a98b90098680c66f2ec0
omarPerez1987 <omar.perez.dev@gmail.com>
2024-02-20 12:07:04 +0100

    first commit

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 const CurrentTime = ({ people, cities, countries }) => (
   <div className="currentTime">
+    <h5>Me han faltado las horas por cada pais</h5>
     {people.map((person) => {
       const city = cities.find((city) => city.city_id === person.city_id);
-      const country = countries.find(
-        (country) => country.country_id === city.country_id
-      );
+      const country = countries.find((country) => country.country_id === city.country_id);
+      console.log(country)
+      
+      const timeZone = country ? country.timeZone : null;
       const currentTime = new Date().toLocaleTimeString("en-US", {
-        timeZone: country.timeZone,
+        timeZone: timeZone,
       });
       return (
+        <>
         <div key={person.person_id}>
           <p>{`${person.name} ${person.surname1}: ${currentTime}`}</p>
         </div>
+        </>
       );
     })}
   </div>
