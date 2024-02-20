899454ccaf544c5ea65cae94598036d0f9d47514
omarPerez1987 <omar.perez.dev@gmail.com>
2024-02-20 16:05:50 +0100

    modificación mostrando horarios según pais ( fuera de horario de prueba )

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,26 +1,54 @@
 import React from "react";
+import moment from "moment-timezone";
 
-const CurrentTime = ({ people, cities, countries }) => (
-  <div className="currentTime">
-    <h5>Me han faltado las horas por cada pais</h5>
-    {people.map((person) => {
-      const city = cities.find((city) => city.city_id === person.city_id);
-      const country = countries.find((country) => country.country_id === city.country_id);
-      console.log(country)
-      
-      const timeZone = country ? country.timeZone : null;
-      const currentTime = new Date().toLocaleTimeString("en-US", {
-        timeZone: timeZone,
-      });
-      return (
-        <>
-        <div key={person.person_id}>
-          <p>{`${person.name} ${person.surname1}: ${currentTime}`}</p>
-        </div>
-        </>
-      );
-    })}
-  </div>
-);
+const CurrentTime = ({ people, cities, countries }) => {
+  const getCurrentTime = (city) => {
+    if (city === "Pamplona") {
+      const timeZone = moment.tz("Europe/Madrid").format('HH:mm:ss');
+      return timeZone;
+    }
+    if (city === "Burdeos") {
+      const timeZone = moment.tz("Europe/Paris").format('HH:mm:ss');
+      return timeZone;
+    }
+
+    if (city === "Santa Cruz de Tenerife") {
+      const timeZone = moment.tz("Atlantic/Canary").format('HH:mm:ss');
+      return timeZone;
+    }
+
+    if (city === "Tokyo") {
+      const timeZone = moment.tz("Asia/Tokyo").format('HH:mm:ss');
+      return timeZone;
+    }
+    if (city === "Bogotá") {
+      const timeZone = moment.tz("America/Colombia").format('HH:mm:ss');
+      return timeZone;
+    }
+  };
+
+  return (
+    <div className="currentTime">
+      <h5>Hora actual por cada país:</h5>
+
+      {people.map((person) => {
+        const city = cities.find((city) => city.city_id === person.city_id);
+        if (!city) return null;
+        const country = countries.find(
+          (country) => country.country_id === city.country_id
+        );
+        if (!country) return null;
+        console.log(city);
+        const currentTime = getCurrentTime(city.cityName);
+
+        return (
+          <div key={person.person_id}>
+            <p>{`${person.name} ${person.surname1}: ${currentTime}`}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default CurrentTime;
